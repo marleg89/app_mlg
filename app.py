ebc35db5a45d5067f2a83ecb9d7b08af5e1e91bd
marleg89 <marien89.lg@gmail.com>
2022-05-13 10:53:35 +0000

    wout_port

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
+app.debug = True
 #@app.route('/')
+
+
 
 @app.route("/")
 def index():
@@ -25,6 +27,5 @@ def page_a_propos():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000)
+    app.run(host='0.0.0.0')
 
-app.run(debug=True)
