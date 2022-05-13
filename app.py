e11fc2d568cb576a420525483a8828838b5111b3
marleg89 <marien89.lg@gmail.com>
2022-05-13 09:55:36 +0000

    Sofien_app

--- app.py
+++ app.py
@@ -1,25 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+#@app.route('/')
+
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 def hello():
-
     return 'Hello World!\n'
 
-
-
-# Route dynamique
-
 @app.route('/hello/<username>')
-
 def hello_user(username):
+    return 'Salut %s! :) \n' % username
 
-    return 'Why Hello %s!\n' % username
+@app.route("/contact")
+def page_contact():
+    return render_template("contact.html")
 
+@app.route("/a-propos")
+def page_a_propos():
+    return render_template("a-propos.html")
 
 
 if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
 
-    app.run(host='0.0.0.0')
+app.run(debug=True)
